2867640d9def9ca1cc7cc65bf6db89ffe85fd96e
Thomas Boigner <thomasboigner@outlook.com>
2026-03-18 19:40:51 +0100

    Update spring-boot-htmx to version 5.1.0

--- src/main/resources/static/js/add-csrf-token.js
+++
@@ -1,5 +0,0 @@
-document.body.addEventListener('htmx:configRequest', function(event) {
-    let token = document.querySelector('meta[name="_csrf"]').content;
-    let header = document.querySelector('meta[name="_csrf_header"]').content;
-    event.detail.headers[header] = token;
-});
