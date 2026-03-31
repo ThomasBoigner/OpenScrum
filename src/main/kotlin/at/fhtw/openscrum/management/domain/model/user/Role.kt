b8d1dafccf4c79493170666ce1169fd7dee48e29
Thomas Boigner <thomasboigner@outlook.com>
2026-03-31 17:46:48 +0200

    Add RoleDto

--- src/main/kotlin/at/fhtw/openscrum/management/domain/model/user/Role.kt
+++ src/main/kotlin/at/fhtw/openscrum/management/domain/model/user/Role.kt
@@ -2,9 +2,8 @@ package at.fhtw.openscrum.management.domain.model.user
 
 enum class Role(
     val roleName: String,
-    val displayName: String,
     val isManager: Boolean,
 ) {
-    USER("ROLE_USER", "User", false),
-    MANAGER("ROLE_MANAGER", "Manager", true),
+    USER("ROLE_USER", false),
+    MANAGER("ROLE_MANAGER", true),
 }
