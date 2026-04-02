25ba0c7911a587fd450e7479132cd4eb224b9655
Thomas Boigner <thomasboigner@outlook.com>
2026-04-02 16:44:43 +0200

    Refactor createProject

--- src/main/kotlin/at/fhtw/openscrum/management/domain/model/project/ProjectService.kt
+++ src/main/kotlin/at/fhtw/openscrum/management/domain/model/project/ProjectService.kt
@@ -1,36 +1,35 @@
 package at.fhtw.openscrum.management.domain.model.project
 
-import at.fhtw.openscrum.management.domain.model.user.Role
 import at.fhtw.openscrum.management.domain.model.user.User
-import at.fhtw.openscrum.management.domain.model.user.UserService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 class ProjectService(
     private val projectRepository: ProjectRepository,
-    private val log: Logger = LoggerFactory.getLogger(UserService::class.java),
+    private val log: Logger = LoggerFactory.getLogger(ProjectService::class.java),
 ) {
     fun createProject(
         authenticatedUser: User,
         projectName: String,
         productOwner: User?,
         scrumMaster: User?,
-        developers: Set<User?>,
+        developers: Set<User>,
     ): Project {
-        require(authenticatedUser.role == Role.MANAGER) { "Only managers can create projects!" }
-        require(productOwner != null) { "Product owner must not be null!" }
-        require(scrumMaster != null) { "Scrum master must not be null!" }
-        require(!projectRepository.existsByProjectName(projectName)) { "Project name is already taken!" }
+        log.debug("Trying to create project {}", projectName)
+        require(authenticatedUser.role.isManager) { "Management permissions are needed!" }
+        require(productOwner != null) { "Product owner does not exist!" }
+        require(scrumMaster != null) { "Scrum master does not exist!" }
+        require(!projectRepository.existsByProjectName(projectName)) { "Project with name $projectName already exists!" }
 
-        log.debug("Creating project '$projectName'")
+        val project =
+            Project(
+                projectName = projectName,
+                productOwnerId = productOwner.userId,
+                scrumMasterId = scrumMaster.userId,
+                developerIds = developers.map { it.userId }.toSet(),
+            )
 
-        val project = Project(
-            projectName = projectName,
-            productOwnerId = productOwner.userId,
-            scrumMasterId = scrumMaster.userId,
-            developerIds = developers.filterNotNull().map { it.userId }.toSet(),
-        )
-
+        log.info("Created Project {}", project)
         return projectRepository.save(project)
     }
 }
--- src/test/kotlin/at/fhtw/openscrum/management/domain/model/project/ProjectServiceTest.kt
+++ src/test/kotlin/at/fhtw/openscrum/management/domain/model/project/ProjectServiceTest.kt
@@ -7,6 +7,7 @@ import at.fhtw.openscrum.management.domain.model.user.User
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
@@ -84,5 +85,256 @@ class ProjectServiceTest {
         assertThat(project.productOwnerId).isEqualTo(productOwner.userId)
         assertThat(project.scrumMasterId).isEqualTo(scrumMaster.userId)
         assertThat(project.developerIds).containsExactly(developer.userId)
+    }
+
+    @Test
+    fun ensureCreateProjectThrowsExceptionWhenUserIsNotAManager() {
+        // Given
+        val projectName = "OpenScrum"
+
+        val user =
+            User(
+                username = "user",
+                emailAddress = EmailAddress("user@gmail.com"),
+                fullName = FullName("Regular", "User"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        val productOwner =
+            User(
+                username = "productOwner",
+                emailAddress = EmailAddress("product.owner@gmail.com"),
+                fullName = FullName("Product", "Owner"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        val scrumMaster =
+            User(
+                username = "scrumMaster",
+                emailAddress = EmailAddress("scrum.master@gmail.com"),
+                fullName = FullName("Scrum", "Master"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        val developer =
+            User(
+                username = "developer",
+                emailAddress = EmailAddress("developer@gmail.com"),
+                fullName = FullName("Dev", "Eloper"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        // When
+        assertThrows<IllegalArgumentException> {
+            projectService.createProject(
+                authenticatedUser = user,
+                projectName = projectName,
+                productOwner = productOwner,
+                scrumMaster = scrumMaster,
+                developers = setOf(developer),
+            )
+        }
+    }
+
+    @Test
+    fun ensureCreateProjectThrowsExceptionWhenProductOwnerIsMissing() {
+        // Given
+        val projectName = "OpenScrum"
+
+        val manager =
+            User(
+                username = "manager",
+                emailAddress = EmailAddress("manager@gmail.com"),
+                fullName = FullName("Manager", "User"),
+                password = "password",
+                role = Role.MANAGER,
+            )
+
+        val scrumMaster =
+            User(
+                username = "scrumMaster",
+                emailAddress = EmailAddress("scrum.master@gmail.com"),
+                fullName = FullName("Scrum", "Master"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        val developer =
+            User(
+                username = "developer",
+                emailAddress = EmailAddress("developer@gmail.com"),
+                fullName = FullName("Dev", "Eloper"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        // When
+        assertThrows<IllegalArgumentException> {
+            projectService.createProject(
+                authenticatedUser = manager,
+                projectName = projectName,
+                productOwner = null,
+                scrumMaster = scrumMaster,
+                developers = setOf(developer),
+            )
+        }
+    }
+
+    @Test
+    fun ensureCreateProjectThrowsExceptionWhenScrumMasterIsMissing() {
+        // Given
+        val projectName = "OpenScrum"
+
+        val manager =
+            User(
+                username = "manager",
+                emailAddress = EmailAddress("manager@gmail.com"),
+                fullName = FullName("Manager", "User"),
+                password = "password",
+                role = Role.MANAGER,
+            )
+
+        val productOwner =
+            User(
+                username = "productOwner",
+                emailAddress = EmailAddress("product.owner@gmail.com"),
+                fullName = FullName("Product", "Owner"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        val developer =
+            User(
+                username = "developer",
+                emailAddress = EmailAddress("developer@gmail.com"),
+                fullName = FullName("Dev", "Eloper"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        // When
+        assertThrows<IllegalArgumentException> {
+            projectService.createProject(
+                authenticatedUser = manager,
+                projectName = projectName,
+                productOwner = productOwner,
+                scrumMaster = null,
+                developers = setOf(developer),
+            )
+        }
+    }
+
+    @Test
+    fun ensureCreateProjectThrowsExceptionWhenProjectNameIsAlreadyTaken() {
+        // Given
+        val projectName = "OpenScrum"
+
+        val manager =
+            User(
+                username = "manager",
+                emailAddress = EmailAddress("manager@gmail.com"),
+                fullName = FullName("Manager", "User"),
+                password = "password",
+                role = Role.MANAGER,
+            )
+
+        val productOwner =
+            User(
+                username = "productOwner",
+                emailAddress = EmailAddress("product.owner@gmail.com"),
+                fullName = FullName("Product", "Owner"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        val scrumMaster =
+            User(
+                username = "scrumMaster",
+                emailAddress = EmailAddress("scrum.master@gmail.com"),
+                fullName = FullName("Scrum", "Master"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        val developer =
+            User(
+                username = "developer",
+                emailAddress = EmailAddress("developer@gmail.com"),
+                fullName = FullName("Dev", "Eloper"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        whenever(projectRepository.existsByProjectName(projectName)).thenReturn(true)
+
+        // When
+        assertThrows<IllegalArgumentException> {
+            projectService.createProject(
+                authenticatedUser = manager,
+                projectName = projectName,
+                productOwner = productOwner,
+                scrumMaster = scrumMaster,
+                developers = setOf(developer),
+            )
+        }
+    }
+
+    @Test
+    fun ensureCreateProjectThrowsExceptionWhenProjectNameIsBlank() {
+        // Given
+        val projectName = ""
+
+        val manager =
+            User(
+                username = "manager",
+                emailAddress = EmailAddress("manager@gmail.com"),
+                fullName = FullName("Manager", "User"),
+                password = "password",
+                role = Role.MANAGER,
+            )
+
+        val productOwner =
+            User(
+                username = "productOwner",
+                emailAddress = EmailAddress("product.owner@gmail.com"),
+                fullName = FullName("Product", "Owner"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        val scrumMaster =
+            User(
+                username = "scrumMaster",
+                emailAddress = EmailAddress("scrum.master@gmail.com"),
+                fullName = FullName("Scrum", "Master"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        val developer =
+            User(
+                username = "developer",
+                emailAddress = EmailAddress("developer@gmail.com"),
+                fullName = FullName("Dev", "Eloper"),
+                password = "password",
+                role = Role.USER,
+            )
+
+        whenever(projectRepository.existsByProjectName(projectName)).thenReturn(false)
+
+        // When
+        assertThrows<IllegalArgumentException> {
+            projectService.createProject(
+                authenticatedUser = manager,
+                projectName = projectName,
+                productOwner = productOwner,
+                scrumMaster = scrumMaster,
+                developers = setOf(developer),
+            )
+        }
     }
 }
