ade1445bd97eb9ba9801ccb52a2ae4f795fedcfc
Thomas Boigner <thomasboigner@outlook.com>
2026-05-11 23:12:22 +0200

    Add deleteProductBacklogItem to ProductBacklogItemApplicationService

---
+++ src/main/kotlin/at/fhtw/openscrum/scrum/application/command/DeleteProductBacklogItemCommand.kt
@@ -0,0 +1,9 @@
+package at.fhtw.openscrum.scrum.application.command
+
+import java.util.UUID
+
+class DeleteProductBacklogItemCommand(
+    val projectId: UUID,
+    val productBacklogItemId: UUID
+) {
+}
